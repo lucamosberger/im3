aa06be542a1b452791708070c0120ed9cf4446bd
lucamosberger <lucamosb@gmail.com>
2024-09-30 14:50:51 +0200

    Bermuda Dreieck

--- js/script.js
+++ js/script.js
@@ -3,3 +3,25 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+// hier ist das medienhaus
+var marker = L.marker([46.84822092641077, 9.501919346543152]).addTo(map);
+
+// hier ist die beste wg no cap
+var circle = L.circle([46.86311270734969, 9.53343707562261], {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 100
+}).addTo(map);
+
+// das coop bermuda dreieck von chur
+var polygon = L.polygon([
+    [46.84709138840044, 9.508265305732754],
+    [46.85245320233996, 9.534896215172118],
+    [46.85950903954763, 9.526837349085262]
+]).addTo(map);
+
+
+
+
