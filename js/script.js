a320517de542486d590f246702dd613e496eb58e
lucamosberger <lucamosb@gmail.com>
2024-09-30 09:38:20 +0200

    nichts geändert lol

---
+++ js/script.js
@@ -0,0 +1,5 @@
+var map = L.map('map').setView([46.8521, 9.5297], 13);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
